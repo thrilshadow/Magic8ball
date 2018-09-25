bba591eaeaecd53cf9b1c857162a0844b3e4a910
Thrilshadow <Thrilshadow85@gmail.com>
2018-09-24 23:03:32 -0400

    Finished magic 8-ball app

---
+++ app/src/main/java/com/example/thrilshadow/magic8_ball/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.thrilshadow.magic8_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ImageView ballDisply = findViewById(R.id.imageEightBall);
+
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+
+        Button myButton;
+
+        myButton = (Button)findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rng = new Random();
+
+                int number = rng.nextInt(5);
+
+                ballDisply.setImageResource(ballArray[number]);
+            }
+        });
+
+    }
+
+}
